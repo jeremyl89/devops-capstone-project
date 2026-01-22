167a39c2e6417f25b00b9f60eb2ee2293b1b2e94
jeremy <jeremylim1989@gmail.com>
2026-01-22 01:13:30 -0500

    1412

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,7 +138,7 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-        def test_get_account_list(self):
+    def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
